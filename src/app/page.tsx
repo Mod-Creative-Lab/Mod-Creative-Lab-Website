b928a6bf8c561b3f49c070c434316104c4de9313
Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>
2024-08-19 02:27:15 +0700

    fix lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Mod from '@/Components/Mod';
+import Image from 'next/image';
 
 import type { Metadata } from 'next';
 
@@ -11,7 +12,13 @@ export default function Home() {
   return (
     <section className='flex h-screen items-center justify-center'>
       <div className='w-1/3'>
-        <img className='w-full' src='/mcl.svg' alt='mcl-icon' />
+        <Image
+          className='w-full object-cover'
+          width={0}
+          height={0}
+          src='/mcl.svg'
+          alt='mcl-icon'
+        />
       </div>
       <Mod width='50' height='50' color='#0cc243' />
     </section>
