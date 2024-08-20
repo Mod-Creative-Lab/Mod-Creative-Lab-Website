9f6b272d4d301fac489150ff27ceb54d653c72db
mina <peemlomsak@gmail.com>
2024-08-20 20:56:01 +0700

    remove randomColor use utils instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,6 @@ export const metadata: Metadata = {
   icons: '/ModHead.png',
 };
 
-function randomColor() {
-  const colorList = [
-    '#14FBA8',
-    '#2614FB',
-    '#FB7E14',
-    '#E9FB14',
-    '#FB1483',
-    '#34FB14',
-    '#7A14FB',
-  ];
-  return colorList[Math.floor(Math.random() * colorList.length)];
-}
-
 export default function Home() {
   return (
     <div>
