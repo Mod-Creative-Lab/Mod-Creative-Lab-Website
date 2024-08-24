3014d81c0e18c5250710b8dfca1f8aeb19edd47a
Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>
2024-08-24 20:27:17 +0700

    add light blur to Section2

--- src/Components/SecondSection.tsx
+++ src/Components/SecondSection.tsx
@@ -23,26 +23,27 @@ export default function SecondSection({}: Props) {
             stiffness: 260,
             damping: 20,
           }}
-          className='absolute left-0 right-0 -z-10 ml-auto mr-auto w-1/3 opacity-30'
+          className='absolute left-0 right-0 -z-20 ml-auto mr-auto w-10/12 opacity-30 xl:w-1/3'
         >
           <ModHeadSVG />
         </motion.div>
+        <div className='absolute left-0 right-0 -z-10 ml-auto mr-auto h-56 w-56 rounded-[50%] bg-[rgba(251,126,20,0.3)] p-[50%] blur-[100px] md:blur-[250px] xl:p-[20%]'></div>
         <div>
           <div className='flex items-center justify-center text-xl md:text-3xl'>
             We are
-            <div className='md::w-48 ml-3 flex w-32 items-center md:ml-6'>
+            <div className='ml-3 flex w-24 items-center md:ml-6 md:w-48'>
               <ModSVG />
             </div>
           </div>
           <div className='mt-7 flex items-center justify-center text-xl md:text-3xl'>
             We aim to build
-            <div className='ml-3 flex w-52 items-center md:ml-6 md:w-72'>
+            <div className='ml-3 flex w-40 items-center md:ml-6 md:w-72'>
               <CreativeSVG />
             </div>
           </div>
           <div className='mt-7 text-center md:text-3xl'>
             <div>
-              <span className='text-2xl font-extrabold md:text-6xl'>LAB</span>
+              <span className='text-3xl font-extrabold md:text-6xl'>LAB</span>
               oratory is where
             </div>
             <div>
